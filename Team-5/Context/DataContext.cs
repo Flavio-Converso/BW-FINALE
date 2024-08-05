7a000ec1251378d05f6146f942c7a454d7015290
Stefanospi <sspitaleri26@gmail.com>
2024-08-05 16:47:54 +0200

    Pharmacy models

--- Team-5/Context/DataContext.cs
+++ Team-5/Context/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Team_5.Models.Auth;
 using Team_5.Models.Clinic;
+using Team_5.Models.Pharmacy;
 
 namespace Team_5.Context
 {
@@ -13,6 +14,13 @@ namespace Team_5.Context
         public virtual DbSet<Owners> Owners { get; set; }
         public virtual DbSet<Roles> Roles { get; set; }
         public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Companies> Companies { get; set; }
+        public virtual DbSet<Drawers> Drawers { get; set; }
+        public virtual DbSet<Lockers> Lockers { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+
+
 
         public DataContext(DbContextOptions<DataContext> opt) : base(opt)
         {
