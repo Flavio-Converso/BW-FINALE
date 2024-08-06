168ca3e4577c4d3425acff811923374f145d3ad2
ziobaga <adrianoleanza01@gmail.com>
2024-08-06 10:06:13 +0200

    examination controller

---
+++ Team-5/Controllers/ExaminationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team_5.Controllers
+{
+    public class ExaminationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
