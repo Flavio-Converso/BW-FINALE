a6a0164f9ab51132ba87d5dc54bcd8ab64cfd168
Ciraldos <andreaciraldo2@gmail.com>
2024-08-08 12:18:31 +0200

    .

---
+++ Team-5/Models/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Team_5.Models.Auth;
+
+namespace Team_5.Models.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public List<Roles>? Roles { get; set; }
+        public Users Users { get; set; }
+    }
+}
