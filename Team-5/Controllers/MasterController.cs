ced3d0d68f3ee4aad5309042b769f9156e01a094
Flavio-Converso <fla.converso@gmail.com>
2024-08-08 16:24:35 +0200

    .

--- Team-5/Controllers/MasterController.cs
+++ Team-5/Controllers/MasterController.cs
@@ -32,11 +32,26 @@ namespace Team_5.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public async Task<IActionResult> CreateRole(Roles role)
         {
-            await _masterSvc.CreateRoleAsync(role);
-            return RedirectToAction("ManageRoles");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Users = await _masterSvc.GetAllUsersWithRolesAsync();
+                ViewBag.Roles = await _masterSvc.GetAllRolesAsync();
+
+                return View("ManageRoles");
+            }
+
+            try
+            {
+                await _masterSvc.CreateRoleAsync(role);
+                return RedirectToAction("ManageRoles");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("CrRole", ex.Message);
+                return View("ManageRoles");
+            }
         }
     }
 }
